cbb94d802227f5bd8c51bc3bd92c5407d3901b90
Kamilo Arroyave Gomez <93850511+KAMBLACK66@users.noreply.github.com>
2023-05-06 23:09:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const switchInput = document.getElementById("my-switch");
 const second = document.getElementById("my-switch-2");
 const bk = document.getElementById("valueIs-1");
 const valueIs2 = document.getElementById("valueIs-2");
+const cColors = document.getElementById("colors");
 
 const playSound = (element, soundUrl) => {
   const audio = new Audio(soundUrl);
@@ -26,4 +27,8 @@ playSound(
 playSound(
   valueIs2,
   "https://res.cloudinary.com/diqy9grnq/video/upload/v1683428140/220206__gameaudio__beep-space-button_mve01g.mp3"
+);
+playSound(
+  cColors,
+  "https://res.cloudinary.com/diqy9grnq/video/upload/v1683432226/19989__acclivity__fingerplop4_ebbnrk.mp3"
 );
