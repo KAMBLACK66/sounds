06e3de0e1f050114fa46cfd4506e445d6ea760a7
Kamilo Arroyave Gomez <93850511+KAMBLACK66@users.noreply.github.com>
2023-05-06 23:18:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const switchInput = document.getElementById("my-switch");
+const second = document.getElementById("my-switch-2");
+const bk = document.getElementById("valueIs-1");
+const valueIs2 = document.getElementById("valueIs-2");
+
+const playSound = (element, soundUrl) => {
+  const audio = new Audio(soundUrl);
+
+  element.addEventListener("change", () => {
+    audio.play();
+  });
+};
+
+playSound(
+  switchInput,
+  "https://res.cloudinary.com/diqy9grnq/video/upload/v1683424286/215772__otisjames__click_lhqjk1.mp3"
+);
+playSound(
+  second,
+  "https://res.cloudinary.com/diqy9grnq/video/upload/v1683426470/629020__kolombooo__button-click_rjgbr3.mp3"
+);
+playSound(
+  bk,
+  "https://res.cloudinary.com/diqy9grnq/video/upload/v1683428140/220206__gameaudio__beep-space-button_mve01g.mp3"
+);
+playSound(
+  valueIs2,
+  "https://res.cloudinary.com/diqy9grnq/video/upload/v1683428140/220206__gameaudio__beep-space-button_mve01g.mp3"
+);
